ff2869fa3110bf6dd5930fb486fc3dd2c7268039
Ernesto Mancebo <ernesmancebo@gmail.com>
2017-09-28 22:28:41 -0400

    initial commit

---
+++ src/main/java/com/ernestomancebo/masteringspring/MasteringSpringApplication.java
@@ -0,0 +1,12 @@
+package com.ernestomancebo.masteringspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringApplication.class, args);
+	}
+}
